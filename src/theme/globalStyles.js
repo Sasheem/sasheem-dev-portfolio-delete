6091da6469332ba221add61a23978a47506f5de4
Sasheem Snell <sasheem.snell@gmail.com>
2021-03-10 23:24:24 -0500

    removed import, causes error

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -1,11 +1,10 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap');
   body {
     padding: 0;
     margin: 0;
-    font-family: Roboto, -apple-system, Roboto, sans-serif, serif;
+    font-family: -apple-system, Roboto, sans-serif, serif;
   }
   a {
     text-decoration: none;
